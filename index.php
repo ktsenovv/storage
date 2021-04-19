d84d0f55bab380e4fa0d11ca9b76ccace9580e45
kcenow <40594589+kcenow@users.noreply.github.com>
2021-04-19 14:34:25 +0300

    Update

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ include('engine/header.php'); #include <HEADER>
 	<div class="head"><?=PAGE_NAME;?></div>
 	<div style="text-align: justify;">
 		Welcome to <?=$g_storage_name;?>. This project is designed to imitate storage of computer shop. You can add, edit and delete suppliers, orders from suppliers, clients, orders from clients and to approve deliveries.
-		<br /><br />The following languages are used for the project creation: HTML, CSS, PHP, jQuery (Javascript library) and SQL.
+		<br /><br />The following languages are used for the project creation: HTML, CSS, PHP, jQuery and SQL.
 	</div>
 </div>
 <?php
